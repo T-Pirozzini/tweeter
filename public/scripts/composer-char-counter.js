143a6bcb3de002fdb0718407e3ccf00adb0e1b73
vagrant <tpirozzini@gmail.com>
2022-03-02 02:02:44 +0000

    adjust counter js logic

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,11 +3,16 @@ $(() => {
 
   const $tweetText = $('textarea');  
 
-  $tweetText.on("input", function() {   
-    this.form.counter.value--;
-    if (this.form.counter.value < 0) {
+  $tweetText.on('input', function() {    
+    let textLength = $(this).val().length;    
+    let maxLength = 10;
+    $(this).siblings("div").children(".counter").text(maxLength - textLength)
+    
+    if (textLength < maxLength) {
+      this.form.counter.classList.remove('counter-red');
+    } else if (textLength > maxLength) {      
       this.form.counter.classList.add('counter-red');
-    }
-  });  
-
+    }   
+  });
 });
+
