acbdbfc5744f5a19703926ca25e59807e2f31aa5
T-Pirozzini <90930100+T-Pirozzini@users.noreply.github.com>
2022-05-26 13:47:03 -0700

    reset

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   mode: 'production',
   entry: './src/index.js',
   output: {
-    filename: 'client.js',
+    filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
